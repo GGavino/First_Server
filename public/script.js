022120d58d10db0cdc5d5556b39c70a0dd65d87a
GGavino <gabriel.gavina07@gmail.com>
2024-10-24 13:43:46 +0100

    hope this is the last commit

--- public/script.js
+++ public/script.js
@@ -1,23 +1,22 @@
 // Function to assign random numbers to each box
 function assignRandomValues() {
     // Example fetch request to retrieve the JSON data from the server
-    fetch('http://localhost:3000/api/generate-key')
+    fetch('https://first-server-klv7.onrender.com/api/generate-key') // Update the URL to match the deployed server URL
     .then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json(); // Parse the response as JSON
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the response as JSON
     })
     .then(data => {
-    console.log('Received data:', data);
-    for (let i = 1; i <= 7; i++) {
-        document.getElementById(`box${i}`).textContent = data[`numero${i}`];
-    }
+        console.log('Received data:', data);
+        for (let i = 1; i <= 7; i++) {
+            document.getElementById(`box${i}`).textContent = data[`numero${i}`];
+        }
     })
     .catch(error => {
-    console.error('Fetch error:', error);
+        console.error('Fetch error:', error);
     });
-
 }
 
 // Attach event listener to the button
